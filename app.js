0f416ade29b0be83740ef90eaacde649e1764533
JillGlass <glass.nz@gmail.com>
2020-06-14 22:08:10 +1200

    add Map for street classification

--- app.js
+++ app.js
@@ -26,51 +26,31 @@ class Element {
     }
 }
 
-class Parks extends Element {
+class Park extends Element {
     constructor(name, buildYear, area, numTrees) {
         super(name, buildYear)
             this.area = area;
             this.numTrees = numTrees;
         }   
+        treeDensity() {
+            const density = this.numTrees / this.area;
+            console.log(`${this.name} has a tree density of ${density} trees per square km`)
+        }
 }
 
-class Streets extends Element {
-    constructor(name, buildYear, length, size) {
+class Street extends Element {
+    constructor(name, buildYear, length, size = 3) {
         super(name, buildYear)
             this.length = length;
             this.size = size;
     }
-}
-
-const allParks = [new Park('Green Park', 1987, 0.2, 215),
-                 new Park('National Park', 1894, 2.9, 3541),
-                 new Park('Oak Park', 1953, 0.4, 949)];
-
-const allStreets = [new Street('Ocean Avenue', 1999, 1.1, 4),
-                   new Street('Evergreen Street', 2008, 2.7, 2),
-                   new Street('4th Street', 2015, 0.8),
-                   new Street('Sunset Boulevard', 1982, 2.5, 5)];
-
-function treeDensity() {
-    //number of trees/park area
-    const density = this.numTrees / this.area;
-    console.log(`The tree density of ${this.name} is ${density} km squared`)
-}
-
-function avParkAge() {
-    //sum of all ages/number of parks
-    const average = this.ages / this.numParks;
-    console.log(`The average age of ${this.name} is ${average} years`)
-}
-
-function moreThan1000() {
-    //park that has more than 1000 trees
-    //loop through parks
-    const trees = this.numTrees.map(tree => {
-        if(this.numTrees >= 1000) {
-            console.log(`${this.name} has more than 1000 trees`)
-        }
+    classifyStreet() {
+        const classification = new Map();
+            classification.set(1,'tiny');
+            classification.set(2,'small');
+            classification.set(3,'normal');
+            classification.set(4,'big');
+            classification.set(5,'huge');
+console.log(`${this.name}, built in ${this.buildYear}, is a ${classification.get(this.size)} street.`)
     }
-        )
-    
 }
